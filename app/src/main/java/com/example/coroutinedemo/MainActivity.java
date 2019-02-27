7957ce080f4da4c7cefbd781c77523fb824bb101
Henry.Chen <henry.chen@twdt191.ict888.net>
2019-02-27 14:11:06 +0800

    With correct result

--- app/src/main/java/com/example/coroutinedemo/MainActivity.java
+++ app/src/main/java/com/example/coroutinedemo/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -39,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
             default:
                 getSupportFragmentManager().popBackStack();
-                Toast.makeText(this, "Name: " + name + ", Age: " + age, Toast.LENGTH_LONG);
+                Toast.makeText(this, "Name: " + name + ", Age: " + age, Toast.LENGTH_LONG).show();
                 break;
         }
     }
@@ -62,37 +63,50 @@ public class MainActivity extends AppCompatActivity {
         @Nullable
         @Override
         public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.fragment, container, false);
+            final View view = inflater.inflate(R.layout.fragment, container, false);
             TextView label = view.findViewById(R.id.label);
             label.setText("Your name?");
             Button button = view.findViewById(R.id.button);
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(getContext(), "FragName Button clicked", Toast.LENGTH_LONG).show();
-                    ((MainActivity)getActivity()).switchToFragment(2);
+                    //Toast.makeText(getContext(), "FragName Button clicked", Toast.LENGTH_LONG).show();
+                    EditText editText = view.findViewById(R.id.content);
+                    getMainActivity().setName(editText.getText().toString());
+                    getMainActivity().switchToFragment(2);
                 }
+
             });
             return view;
         }
 
+        private MainActivity getMainActivity() {
+            return (MainActivity)getActivity();
+        }
     }
 
     public static class FragAge extends Fragment {
         @Nullable
         @Override
         public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            View view = inflater.inflate(R.layout.fragment, container, false);
+            final View view = inflater.inflate(R.layout.fragment, container, false);
             TextView label = view.findViewById(R.id.label);
             label.setText("Your age?");
             Button button = view.findViewById(R.id.button);
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(getContext(), "FragAge Button clicked", Toast.LENGTH_LONG).show();
+                    //Toast.makeText(getContext(), "FragAge Button clicked", Toast.LENGTH_LONG).show();
+                    EditText editText = view.findViewById(R.id.content);
+                    getMainActivity().setAge(editText.getText().toString());
+                    getMainActivity().switchToFragment(3);
                 }
             });
             return view;
+        }
+
+        private MainActivity getMainActivity() {
+            return (MainActivity)getActivity();
         }
     }
 
